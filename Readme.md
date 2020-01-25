d6fb1f96cd477e66538faca743c64231be6186a2
Nikddimos Gebregiorgis <gebrenick07@gmail.com>
2020-01-24 18:56:23 -0700

    updating server.js

--- Readme.md
+++ Readme.md
@@ -1,1 +1,3 @@
-# Note Taker 
+# Note Taker
+
+This is a basic notetaking app that allows the user to write new notes with a header and body text, as well as delete those notes upon completion.
--- server.js
+++ server.js
@@ -1,24 +1,39 @@
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
 
 // Sets up the Express App
 // =============================================================
 var app = express();
 var PORT = process.env.PORT || 3000;
 
+const dbJSON = []
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-
+//HTML routes:
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/api/notes", function(req, res){
+    res.json(dbJSON);
 });
 
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
+
+app.post ("/api/notes", function (req, res) {
+    let newNote = req.body;
+    newNote.id = newNote.title.split(" ").join("").toLowerCase()
+    dbJSON.push(newNote);
+    console.log(newNote)
+    console.log(dbJSON)
+    res.json(newNote)
+});
+
 
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);

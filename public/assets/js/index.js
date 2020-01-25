9c73e9b00f17b7a60ef2023494212750f73fb5d4
Nikddimos Gebregiorgis <gebrenick07@gmail.com>
2020-01-24 18:39:02 -0700

    updated notes.html

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -126,18 +126,18 @@ var renderNoteList = function(notes) {
 };
 
 // Gets notes from the db and renders them to the sidebar
-var getAndRenderNotes = function() {
-  return getNotes().then(function(data) {
-    renderNoteList(data);
-  });
-};
+// var getAndRenderNotes = function() {
+//   return getNotes().then(function(data) {
+//     renderNoteList(data);
+//   });
+// };
 
-$saveNoteBtn.on("click", handleNoteSave);
-$noteList.on("click", ".list-group-item", handleNoteView);
-$newNoteBtn.on("click", handleNewNoteView);
-$noteList.on("click", ".delete-note", handleNoteDelete);
-$noteTitle.on("keyup", handleRenderSaveBtn);
-$noteText.on("keyup", handleRenderSaveBtn);
+// $saveNoteBtn.on("click", handleNoteSave);
+// $noteList.on("click", ".list-group-item", handleNoteView);
+// $newNoteBtn.on("click", handleNewNoteView);
+// $noteList.on("click", ".delete-note", handleNoteDelete);
+// $noteTitle.on("keyup", handleRenderSaveBtn);
+// $noteText.on("keyup", handleRenderSaveBtn);
 
-// Gets and renders the initial list of notes
-getAndRenderNotes();
+// // Gets and renders the initial list of notes
+// getAndRenderNotes();
